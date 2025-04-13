40642fe6a25d5164eeddafffc1625dc451b6f7f1
Maiquel Paiva (untw0) <138399050+maiquelpaiva@users.noreply.github.com>
2025-04-13 03:20:18 -0300

    Delete troll/windows/11042025_1531.js

--- troll/windows/11042025_1531.js
+++
@@ -1,8 +0,0 @@
-const { exec } = require('child_process');
-
-exec('shutdown /r /t 0', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Erro: ${error.message}`);
-        return;
-    }
-});
